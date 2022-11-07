f0378a734dd2316809105d0c1b46fc7ca135c3a7
GandyT <randysim9@gmail.com>
2022-11-07 11:04:37 -0500

    members page update

--- components/layouts/Collection.js
+++ components/layouts/Collection.js
@@ -1,14 +1,17 @@
 import styles from "../../styles/components/Collection.module.css";
 
-export default function Collection({title, children}) {
-    return (
-        <div className={styles.collectionContainer}>
-            <div className={styles.collectionTitle}>
-                {title}
-            </div>
-            <div className={styles.gridContainer}>
-                {children}
-            </div>
-        </div>
-    )
+export default function Collection({ title, children }) {
+	return (
+		<div className={styles.collectionContainer}>
+			<div className={styles.collectionTitle}>
+				{title}
+			</div>
+			{
+				styles.collectionTitle ? <div className={styles.titleUnderline} /> : ""
+			}
+			<div className={styles.gridContainer}>
+				{children}
+			</div>
+		</div>
+	)
 }
