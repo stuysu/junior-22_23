6160d7768f8dfecbe189de8c6b7a110c42b02ff5
Randy Sim <randysim9@gmail.com>
2022-11-07 22:16:15 -0500

    setup for csv, dynamic member cards

--- pages/members.js
+++ pages/members.js
@@ -5,6 +5,44 @@ import Collection from "../components/layouts/Collection";
 
 import styles from "../styles/Members.module.css";
 
+/* 
+MEMBER IN FORMAT
+[
+    [email, name, position, department, desc, img_src]
+]
+*/
+const renderMembers = members => {
+    let deps = {}
+    members.forEach(member => {
+        if (!deps[member[3]]) deps[member[3]] = [];
+        deps[member[3]].push(member);
+    });
+
+    let collections = [];
+
+    for (let [key, value] of Object.entries(deps)) {
+        let cards = value.map(m => <MemberCard name={m[1]} src={m[5]} role={m[2]}>{m[4]}</MemberCard>)
+        collections.push(
+            <Collection title={key.toUpperCase()}>
+                {cards}
+            </Collection>
+        )
+    }
+
+    return collections;
+}
+
+/* TEMP MEMBERS, create CSV parse function once we get CSV */
+const members = [
+    ["rsim40@stuy.edu", "Randy Sim", "JC President", "IT", "I have no personality nor interests.", ""],
+    ["rsim40@stuy.edu", "Randy Sim", "JC President", "MEDIA", "I have no personality nor interests.", ""],
+    ["rsim40@stuy.edu", "Randy Sim", "JC President", "EVENTS", "I have no personality nor interests.", ""],
+    ["rsim40@stuy.edu", "Randy Sim", "JC President", "FINANCE", "I have no personality nor interests.", ""],
+    ["rsim40@stuy.edu", "Randy Sim", "JC President", "IT", "I have no personality nor interests.", ""],
+    ["rsim40@stuy.edu", "Randy Sim", "JC President", "IT", "I have no personality nor interests.", ""],
+    ["rsim40@stuy.edu", "Randy Sim", "JC President", "IT", "I have no personality nor interests.", ""],
+]
+
 // Temp Navbar. Render once
 // store members somewhere else for quick editing
 export default function Members() {
@@ -20,23 +58,7 @@ export default function Members() {
                 Members
             </div>
             
-            <Collection title="IT">
-                <MemberCard name="Randy Sim" src="">
-                    This is a test description. I have no personality nor interests.
-                </MemberCard>
-                <MemberCard name="Randy Sim" src="">
-                    This is a test description. I have no personality nor interests.
-                </MemberCard>
-                <MemberCard name="Randy Sim" src="">
-                    This is a test description. I have no personality nor interests.
-                </MemberCard>
-                <MemberCard name="Randy Sim" src="">
-                    This is a test description. I have no personality nor interests.
-                </MemberCard>
-                <MemberCard name="Randy Sim" src="">
-                    This is a test description. I have no personality nor interests.
-                </MemberCard>
-            </Collection>
+            {renderMembers(members)}
         </div>
     )
 }
